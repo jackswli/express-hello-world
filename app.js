9ba4c2e9cecbc25013f760e4d27986e0c7ea78a0
Jack Li <jack.sw.li@clsa.com>
2022-10-14 01:07:24 +0000

    adding index.html

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+// app.get("/", (req, res) => res.type('html').send(html));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
