0c05a63a0da52de6103d6ce609a5277f3b14ae9d
Jack Li <jack.sw.li@clsa.com>
2022-10-14 01:17:04 +0000

    adding broadcast and client side script

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // app.get("/", (req, res) => res.type('html').send(html));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.broadcast.emit('hi');
+    socket.on('disconnect', () => { console.log('user disconnected'); });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
