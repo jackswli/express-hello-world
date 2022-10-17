6deba83fc3f2a7ce6c698d7001fb259072b16301
Jack Li <jack.sw.li@clsa.com>
2022-10-17 03:51:32 +0000

    adding route heartbeat

--- app.js
+++ app.js
@@ -6,15 +6,22 @@ const port = process.env.PORT || 3001;
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+user_count = 0;
+
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
 app.get('/heartbeat', (req, res) => {
-    io.emit('chat message', 'Welcome.');
+    io.emit('chat message', 'Welcome ' + user_count + (user_count > 2 ? "th" : (user_count > 1 ? "nd" : (user_count > 0 ? "st" : "th"))) + " user.");
+    res.send("OK");
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    user_count = user_count + 1;
     socket.broadcast.emit('hi');
-    socket.on('disconnect', () => { console.log('user disconnected'); });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        user_count = user_count - 1;
+    });
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
         io.emit('chat message', msg);
